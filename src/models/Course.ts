97c325b411e483750b45bb40bc5e9457aaa9cd33
Thiago Elias <thiagoelias99@gmail.com>
2023-08-20 09:32:20 -0300

    feat: created models

---
+++ src/models/Course.ts
@@ -0,0 +1,7 @@
+export interface ICourse {
+    uuid?: string,
+    description: string,
+    courseHours: number,
+    startDate: Date,
+    finishDate: Date
+}
---
+++ src/models/Student.ts
@@ -0,0 +1,7 @@
+export interface IStudent {
+    uuid?: string,
+    name: string,
+    email: string,
+    password: string,
+    birthDate: Date
+}
