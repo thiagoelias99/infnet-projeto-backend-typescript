dc758890e1639bc3c5438808f6ccd1cf1f0bb51a
Thiago Elias <thiagoelias99@gmail.com>
2023-08-20 11:19:26 -0300

    feat: upgrade to ts

--- src/server/services/CryptServices/index.ts
+++ src/server/services/CryptServices/index.ts
@@ -1,14 +1,14 @@
-const { genSalt, hash, compare } = require("bcrypt");
+import { genSalt, hash, compare } from "bcrypt";
 
 const SALT_RANDOMS = 8;
 
-const hashPassword = async (password) => {
+const hashPassword = async (password: string) => {
     const saltGenerated = await genSalt(SALT_RANDOMS);
 
     return await hash(password, saltGenerated);
 };
 
-const verifyPassword = async (password, hashedPassword) => {
+const verifyPassword = async (password: string, hashedPassword: string) => {
     return await compare(password, hashedPassword);
 };
 
